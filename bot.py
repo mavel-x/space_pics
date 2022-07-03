f7a133084cd64bb33fb35c1aa496ec17e18d0dbd
mavel <pavel@mavel.cc>
2022-07-03 16:23:31 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
 from telegram import Bot
 from dotenv import load_dotenv
 import os
+from pathlib import Path
+from file_operations import get_file_extension
 
 if __name__ == "__main__":
     load_dotenv()
     token = os.getenv('TG_TOKEN')
     chat_id = os.getenv('TG_CHAT_ID')
     bot = Bot(token=token)
-    bot.send_message(chat_id=chat_id, text='Sample text')
+    images_dir = Path('images')
+    images = [x for x in images_dir.iterdir()]
+    image = images[0]
+    with open(image, 'rb') as file:
+        if image.suffix == '.gif':
+            bot.send_animation(chat_id=chat_id, animation=file)
+        else:
+            bot.send_photo(chat_id=chat_id, photo=file)
