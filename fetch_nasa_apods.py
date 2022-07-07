3ce4da25906d982668fba57f0f10eb49fecebd11
mavel <pavel@mavel.cc>
2022-07-08 01:42:20 +0300

    add 'latest' option to 'fetch_nasa_apods.py'

--- fetch_nasa_apods.py
+++ fetch_nasa_apods.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import requests
 from dotenv import load_dotenv
+from datetime import date, timedelta
 from file_operations import save_remote_image, get_file_extension_from_url
 
 
@@ -17,7 +18,25 @@ def fetch_random_apods(api_key, number_of_imgs=10):
     image_links = [apod['url'] for apod in apods if apod['media_type'] == 'image']
     for index, link in enumerate(image_links, 1):
         extension = get_file_extension_from_url(link)
-        save_remote_image(link, f'nasa_apod_{index}{extension}')
+        save_remote_image(link, f'nasa_apod_random_{index}{extension}')
+
+
+def fetch_latest_apods(api_key, number_of_days):
+    start_date = date.today() - timedelta(days=number_of_days)
+    formatted_start_date = start_date.strftime('%Y-%m-%d')
+
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {
+        'api_key': api_key,
+        'start_date': formatted_start_date,
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    apods = response.json()
+    image_links = [apod['url'] for apod in apods if apod['media_type'] == 'image']
+    for index, link in enumerate(image_links, 1):
+        extension = get_file_extension_from_url(link)
+        save_remote_image(link, f'nasa_apod_latest_{index}{extension}')
 
 
 if __name__ == '__main__':
@@ -28,16 +47,32 @@ if __name__ == '__main__':
             'so you may occasionally get fewer images than requested.')
         )
     parser.add_argument(
-        'number_of_imgs',
+        '-r', '--random',
+        dest='n_random_images',
+        metavar="N",
         nargs='?',
-        default=10,
+        const=10,
         type=int,
-        help='Number of images (default: 10).',
+        help='Send N random images (default quantity: 10).',
+    )
+    parser.add_argument(
+        '-l', '--latest',
+        dest='n_latest_images',
+        metavar="N",
+        nargs='?',
+        const=10,
+        type=int,
+        help='Send APODs for the last N days (default number of days: 10).',
     )
     args = parser.parse_args()
 
     load_dotenv()
     api_key = os.getenv('NASA_KEY')
 
-    fetch_random_apods(api_key, args.number_of_imgs)
+    if args.n_random_images:
+        fetch_random_apods(api_key, args.n_random_images)
+    if args.n_latest_images:
+        fetch_latest_apods(api_key, args.n_latest_images)
+    if not args.n_random_images and not args.n_latest_images:
+        fetch_random_apods(api_key)
     print('Images saved.')
